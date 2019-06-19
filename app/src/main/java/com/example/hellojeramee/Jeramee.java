1bb5243baa89f3d666a906d76e52b121906fe601
Jeremy <jeremyfortenberry@Jeremys-MacBook-Pro.local>
2019-06-18 19:37:24 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hellojeramee/Jeramee.java
@@ -0,0 +1,12 @@
+package com.example.hellojeramee;
+
+class Jeramee {
+    public static Object LENGTH_SHORT;
+
+    public static Jeramee makeText(MainActivity mainActivity, int jeramee_message, Object lengthShort) {
+        return null;
+    }
+
+    public void show() {
+    }
+}
